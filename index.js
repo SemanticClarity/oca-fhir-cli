d3f4ab9cc3064583b0b4f5f00b11172fcb1a3bb4
Mukundan Parthasarathy <35391221+mukundanps@users.noreply.github.com>
2020-12-29 10:51:05 -0800

    Complete OCA Bundle base schema generation for CVC

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
-const fhirpath = require('fhirpath');
+var hbs = require('./lib/hbs-utils.js');
+var Handlebars = require("handlebars");
+const fhirpath = require('fhirpath.js');
+
+/*
+
 var Fhir = require('fhir').Fhir;
 var ParseConformance = require('fhir').ParseConformance;
 var FhirVersions = require('fhir').Versions;
+*/
 
 const main = async()=> {
+  /*
   var cvcimmunizationdoc = fs.readFileSync('examples/cvc-immunizationpassport/patient-bundle-instance-example.json');
   var patientpassport = await JSON.parse(cvcimmunizationdoc);
   if (fhirpath.evaluate(patientpassport, 'Bundle.where(type = \'collection\')')) {
@@ -22,6 +29,29 @@ const main = async()=> {
   var fhir = new Fhir(parser);
   var results = fhir.validate(patientpassport, {errorOnUnexpected: false}); 
   console.log(results);
+  */
+  var cvcimmunizationdoc = fs.readFileSync('examples/cvc-immunizationpassport/patient-bundle-instance-example.json');
+  var patientpassport = await JSON.parse(cvcimmunizationdoc);
+  if (fhirpath.evaluate(patientpassport, 'Bundle.where(type = \'collection\')')) {
+    var retval = fhirpath.evaluate(patientpassport, 'Bundle.entry.resourceType');
+    for (var loop = 0; loop < retval.length; loop++) {
+      console.log("retval["+loop+"]: "+ retval[loop]);
+    }
+  }
+  const bundlehbs = fs.readFileSync('templates/cvc-immunization-passport/cvc-immunization-passport-schemabase.hbs', 'utf-8');
+  const compiled_template = Handlebars.compile(bundlehbs);
+  var result = compiled_template(patientpassport,{
+    "data": {
+        "author": "FHIR OCA WG",
+        "umls-cui": "UMLS:C0008960",
+        "role": "TBD",
+        "fhir-profile-ref": "CA-Core",
+        "fhir-profile-uri": "http://hl7.org/fhir/ca/core/StructureDefinition/",
+        "oca-artifact-type": "spec/schema_base/1.0",
+        "dpv-purpose": "dpv:MedicalHealth"
+    }
+    });
+    console.log(result.toString());
   
 }
 
