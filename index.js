2f2bc53f6bffc76e9960794195e3556a612fa33c
Mukundan Parthasarathy <35391221+mukundanps@users.noreply.github.com>
2021-01-18 13:35:05 -0800

    Config support and hbs async issue fix
    -Add config support
    -Fix hbs timing issue

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+
 const fs = require('fs');
-var hbs = require('./lib/hbs-utils.js');
+var path = require('path');
+var os   = require('os');
 var Handlebars = require("handlebars");
+const hbs = require('./lib/hbs-utils');
 const fhirpath = require('fhirpath.js');
 const hashlinkutil = require('./lib/hashlink-utils');
+const ConfigStore = require('configstore');
+const pkg = require('./package.json');
+const { exit } = require('process');
+const { profile } = require('console');
 
 /*
 
@@ -84,7 +91,46 @@ function generateOCAInformationOverlay(immnzpassportdoc, context, schemabase_has
   fs.writeFileSync(`oca-artifacts/information-overlay/${context}-information.jsonld`, result);
 }
 
-const main = async()=> {
+async function initHashlinkCache() {
+  let hlcache = await hashlinkutil.processDirFiles(`./data/oca-primitives/schema-base`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(hlcache);
+    },0);
+  });
+}
+
+async function generateOCAArtifacts(cfg, bundlepath,profile) {
+
+  initHashlinkCache()
+  .then(async (hlcache) => {
+    var context = "oca-"+profile;
+    var fhirbundlefile = fs.readFileSync(bundlepath);
+    var bundleobject = await JSON.parse(fhirbundlefile);
+    let hl = await generateSchemaBase(cfg,context,profile, bundleobject);
+    generateOCAEntryOverlay(bundleobject,context,hl);
+    generateOCAFormatOverlay(bundleobject,context,hl);
+    generateOCAInformationOverlay(bundleobject,context,hl);
+    generateOCACharacterOverlay(bundleobject,context,hl);
+  })
+  .catch((error) => {console.log(error)});
+  
+}
+
+async function generateSchemaBase(configobj, ctx, profile, bundle) {
+  let bundlehbs = fs.readFileSync(`templates/${profile}/${profile}-schemabase.hbs`, 'utf-8');
+  let compiled_template = Handlebars.compile(bundlehbs);
+  let result = compiled_template(bundle,{"data": configobj});
+    
+  let ocafile = `oca-artifacts/schema-base/${ctx}.jsonld`;
+  var schemabasehl;
+  fs.writeFileSync(ocafile, result);
+  schemabasehl = await hashlinkutil.getHashlink(ocafile);
+  console.log(`getHashlink-genSchemaBase(${ocafile}): ${schemabasehl}`);
+  return schemabasehl;
+} 
+
+const main = async() => {
   /*
   var cvcimmunizationdoc = fs.readFileSync('examples/cvc-immunizationpassport/patient-bundle-instance-example.json');
   var patientpassport = await JSON.parse(cvcimmunizationdoc);
@@ -104,31 +150,40 @@ const main = async()=> {
   var results = fhir.validate(patientpassport, {errorOnUnexpected: false}); 
   console.log(results);
   */
-  var cvcimmunizationdoc = fs.readFileSync('examples/cvc-immunizationpassport/patient-bundle-instance-example.json');
-  var patientpassport = await JSON.parse(cvcimmunizationdoc);
+  var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+  let configFilePath = './config/oca-fhir-cli.json';
+  let configStr = fs.readFileSync(configFilePath);
+  var config =  await JSON.parse(configStr);
   
-  const bundlehbs = fs.readFileSync('templates/cvc-immunization-passport/cvc-immunization-passport-schemabase.hbs', 'utf-8');
-  const compiled_template = Handlebars.compile(bundlehbs);
-  var result = compiled_template(patientpassport,{
-    "data": {
-        "author": "FHIR OCA WG",
-        "umls-cui": "UMLS:C0008960",
-        "role": "TBD",
-        "fhir-profile-ref": "CA-Core",
-        "fhir-profile-uri": "http://hl7.org/fhir/ca/core/StructureDefinition/",
-        "oca-artifact-type": "spec/schema_base/1.0",
-        "dpv-purpose": "dpv:MedicalHealth"
+  //console.log(config);
+
+  var profile_name;
+  if (argv.profile) {
+    profile_name = argv.profile;
+    if (!fs.existsSync(`templates/${profile_name}`)) {
+      console.log("Exiting: Unsupported profile: "+ profile_name);
+      exit(1);
     }
-    });
     
-    var context = 'oca-cvc-immunization-passport';
-    fs.writeFileSync(`oca-artifacts/schema-base/${context}.jsonld`, result);
-    var hashlinkstr;
-    hashlinkstr = await hashlinkutil.getHashlink(`oca-artifacts/schema-base/${context}.jsonld`);
-    generateOCAEntryOverlay(patientpassport,context,hashlinkstr);
-    generateOCAFormatOverlay(patientpassport,context,hashlinkstr);
-    generateOCAInformationOverlay(patientpassport,context,hashlinkstr);
-    generateOCACharacterOverlay(patientpassport,context,hashlinkstr);
+  }  else {
+    console.log("Exiting: Please provide profile name using --profile option");
+    exit(1);
+  }
+
+  var fhirbundle;
+  if (argv.input) {
+    fhirbundle = argv.input;
+    if (!fs.existsSync(fhirbundle)) {
+      console.log("Exiting: Input FHIR Bundle does not exist["+fhirbundle+"]");
+      exit(2);
+    }
+  } else {
+    console.log("Exiting: Please provide fhir bundle input file using --input option");
+    exit(3);
+  }
+
+  generateOCAArtifacts(config, fhirbundle, profile_name);
+
 }
 
 main();
